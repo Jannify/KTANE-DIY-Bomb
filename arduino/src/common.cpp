1a28d025c7f6f63ddc41fb99b01aea575d59c45b
Jannify <23176718+Jannify@users.noreply.github.com>
2024-07-02 16:26:53 +0200

    change: Reduce flickering for controller driven LEDs

--- arduino/src/common.cpp
+++ arduino/src/common.cpp
@@ -4,7 +4,7 @@ Timer<20> timer;
 
 TCA9548 multiplexer(0x70);
 
-bool activeModules[11] = { false };
+bool activeModules[11] = {false};
 bool bombStarted = false;
 
 bool loopLogicButtonCooldown = false;
@@ -12,77 +12,83 @@ bool loopSerialWriteCooldown = false;
 
 byte memoryTriesBuffer = 0x00;
 
-void startBomb() {
+void startBomb()
+{
   for (size_t i = 0; i < 6; i++)
   {
     activeModules[i] = true;
   }
 
-    for (size_t i = 6; i < 11; i++)
+  for (size_t i = 6; i < 11; i++)
   {
     activeModules[i] = false;
   }
-  
+
   bombStarted = true;
 }
 
-void engageLogicCooldown() {
+void engageLogicCooldown()
+{
   loopLogicButtonCooldown = true;
   timer.in(50, [](void *) -> bool {
-    loopLogicButtonCooldown = false;
-    return false;
-  });
+      loopLogicButtonCooldown = false;
+      return false; 
+    });
 }
 
-void engageSerialWriteCooldown() {
+void engageSerialWriteCooldown()
+{
   loopSerialWriteCooldown = true;
   timer.in(50, [](void *) -> bool {
-    loopSerialWriteCooldown = false;
-    return false;
-  });
+      loopSerialWriteCooldown = false;
+      return false; 
+    });
 }
 
 // Bitfield of Modules solved (5 zeros + from 10 to 0)
-void setSolvedModules(byte data0, byte data1) {
-    activeModules[10] = data0 & B00000100;
-    activeModules[9] = data0 & B00000010;
-    activeModules[8] = data0 & B00000001;
-    
-    activeModules[7] = data1 & B10000000;
-    activeModules[6] = data1 & B01000000;
-    activeModules[5] = data1 & B00100000;
-    activeModules[4] = data1 & B00010000;
-    activeModules[3] = data1 & B00001000;
-    activeModules[2] = data1 & B00000100;
-    activeModules[1] = data1 & B00000010;
-    activeModules[0] = data1 & B00000001;
+void setSolvedModules(byte data0, byte data1)
+{
+  activeModules[10] = data0 & B00000100;
+  activeModules[9] = data0 & B00000010;
+  activeModules[8] = data0 & B00000001;
+
+  activeModules[7] = data1 & B10000000;
+  activeModules[6] = data1 & B01000000;
+  activeModules[5] = data1 & B00100000;
+  activeModules[4] = data1 & B00010000;
+  activeModules[3] = data1 & B00001000;
+  activeModules[2] = data1 & B00000100;
+  activeModules[1] = data1 & B00000010;
+  activeModules[0] = data1 & B00000001;
 }
 
-bool setPinLow(void *argument) {
+bool setPinLow(void *argument)
+{
   digitalWrite((int)argument, LOW);
   return false;
 }
 
 void shiftOutLED(byte clockPin, byte val)
 {
-    for (byte i = 0; i < 8; i++)
-    {
-        digitalWrite(27, (val & 128) != 0);
-        val <<= 1;
+  for (byte i = 0; i < 8; i++)
+  {
+    digitalWrite(OUTPUT_Register_Data_LED, (val & 128) != 0);
+    val <<= 1;
 
-        digitalWrite(clockPin, HIGH);
-        digitalWrite(clockPin, LOW);
-        delay(2);
-    }
-    delay(3);
+    digitalWrite(clockPin, HIGH);
+    digitalWrite(clockPin, LOW);
+  }
+  digitalWrite(OUTPUT_Register_Data_LED, 0);
+  delay(2);
 }
 
-void reset() {
-  //Reset BigButton
-  //Reset 7Bit
-  //Reset I2C
-  //Reset SPI
+void reset()
+{
+  // Reset BigButton
+  // Reset 7Bit
+  // Reset I2C
+  // Reset SPI
   timer.cancel();
   bombStarted = false;
-  //digitalWrite(OUTPUT_RESET, LOW); //TODO: Evaluate need
+  // digitalWrite(OUTPUT_RESET, LOW); //TODO: Evaluate need
 }
